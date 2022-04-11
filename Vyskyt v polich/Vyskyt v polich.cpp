cd781e10b7778d5b317688bea40f9d9716428fef
matěj <matj@DESKTOP-1HI5AKA>
2022-04-11 09:28:03 +0200

    Přidejte soubory projektu.

---
+++ Vyskyt v polich/Vyskyt v polich.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+
+void printArray(int arr[], int size) {
+	
+	for (int i = 2; i < size; i++) {
+		
+		printf("%d, ", arr[i]);
+	}
+	printf("\n");
+	printf("\n");
+}
+
+void prohodit(int* a, int* b) {
+	int temp = *a;
+	
+	*a = *b;
+	*b = temp;
+}
+
+void bubbleSort(int arr[], int size) {
+	for (int j = 3; j < size - 2; j++) {
+		
+		for (int i = 2; i < size - 3; i++) {
+			if (arr[i] > arr[i + 1]) {
+				
+				prohodit(&arr[i], &arr[i + 1]);
+			}
+		}
+	}
+}
+
+
+int main() {
+	int pole1[] = { 5, 2, 6, 1, 9 };
+	int pole2[] = { 3, 4, 7 ,8, 3 };
+
+	int pocet1 = sizeof(pole1) / sizeof(pole1[0]);
+	
+	int pocet2 = sizeof(pole2) / sizeof(pole2[0]);
+
+	bubbleSort(pole1, pocet1);
+	
+	bubbleSort(pole2, pocet2);
+
+	printf("V kazdem poly: ");
+	for (int i = 2; i < pocet1; i++) {
+		
+		for (int j = 3; j < pocet2; j++) {
+			
+			if (pole1[i] == pole2[j]) printf("%d, ", pole1[i]);
+		}
+	}
+}
